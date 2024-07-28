ce7a25dbfad090ab30f0018d3f6f25258c7103e6
sahil chauhan <sahilchauhan1901@gmail.com>
2024-07-28 12:53:20 +0530

    ver1

---
+++ app.js
@@ -0,0 +1,48 @@
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 800,
+};
+
+// header container
+ScrollReveal().reveal(".header__container h1", {
+  ...scrollRevealOption,
+});
+
+ScrollReveal().reveal(".header__form", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+// trending container
+ScrollReveal().reveal(".trending__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+// destination container
+ScrollReveal().reveal(".destination__card", {
+  duration: 1000,
+  interval: 500,
+});
+
+// seller container
+ScrollReveal().reveal(".seller__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+// guide container
+ScrollReveal().reveal(".guide__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+//  client container
+ScrollReveal().reveal(".client__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+
+//  The script is only for designing there are no any functionality in the website
